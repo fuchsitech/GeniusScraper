4cdc8a778fa44df12f6c9f8be449a3cf2cbcb260
Fuchsi <fuchsitech@gmail.com>
2020-10-30 19:02:50 +0100

    Can now Download Lyrics, save them into Json and process the Data in a new function

--- Main.py
+++ Main.py
@@ -1,6 +1,43 @@
 import Secret
 import lyricsgenius
+import time
+import streamlit as st
+import numpy as np
+import pandas as pd
+import json
+import os
 
-genius = lyricsgenius.Genius(Secret.GENIUS_ACCESS_TOKEN)
-artist = genius.search_artist("Andy Shauf", max_songs=3, sort="title")
-print(artist.songs)
+
+def get_lyrics(artist):
+    # Create and Configure Scraper
+    genius = lyricsgenius.Genius(Secret.GENIUS_ACCESS_TOKEN)
+    genius.verbose = True
+    genius.remove_section_headers = False
+    genius.skip_non_songs = True
+    genius.excluded_terms = ["(Remix)", "(Snippet)"]
+
+    # Create Artist Object, including all Songs
+    result = genius.search_artist(artist, sort="title")
+    artist.result()
+
+
+def process_data(file):
+    with open(file) as json_file:
+        data = json.load(json_file)
+    print(data)
+    # Now somehow Process the data :)
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    artist_list = ["Kollegah", "Ufo361"]
+
+    for artist in artist_list:
+        file = "Lyrics_{}.json".format(artist)
+        if not os.path.isfile(file):
+            get_lyrics(artist)
+        process_data(file)
+
+
+
+
